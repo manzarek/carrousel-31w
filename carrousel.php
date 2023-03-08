35d7912d1a599a78e23f1258737df02a632826ba
Matteau-Pelletier <e2196113@cmaisonneuve.qc.ca>
2023-03-08 10:56:38 -0500

    c11 début du carrousel

---
+++ carrousel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Extension carrousel permet d'afficher dans une boîte modale
+ * les images d'une galerie
+ * Package name : Carrousel
+ * Version: 1.0.0
+*/
+/**
+ * Plugin Name: Carrousel
+ * Author: Carl Matteau-Pelletier
+ * Plugin URI: https://github.com/manzarek/carrousel
+ * Description: Permet d'afficher dans une boîte modaler  les images d'une galerie
+ */
+
