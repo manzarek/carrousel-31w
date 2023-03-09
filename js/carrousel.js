69281ad6399fa7d223055e5fe84fd1803f363022
Matteau-Pelletier <e2196113@cmaisonneuve.qc.ca>
2023-03-09 15:22:28 -0500

    c12 ouverture de la boite modale

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,4 +1,17 @@
 (function(){
 // fonction IFEE
 console.log('début du carrousel');
+let bouton__ouvrir = document.querySelector('.bouton__ouvrir');
+let elmCarrousel = document.querySelector('.carrousel');
+let elmBtn__x = document.querySelector('.bouton__x');
+console.log(bouton__ouvrir.tagName);
+bouton__ouvrir.addEventListener('mousedown', function(){
+    console.log('boite modale');
+    elmCarrousel.classList.add('carrousel--ouvrir');
+})
+elmBtn__x.addEventListener('mousedown', function() {
+    console.log('boite modale');
+    elmCarrousel.classList.remove('carrousel--ouvrir');
+})
+
 })()
